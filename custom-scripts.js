801df89e947659ec653548f270c2298eb350a020
codingdrizzle <codingfrancis100@gmail.com>
2021-11-11 21:07:09 +0000

    almost getting done

--- custom-scripts.js
+++ custom-scripts.js
@@ -1,3 +1,4 @@
 const year = document.querySelector('#year');
 
 year.innerHTML = new Date().getFullYear()
+
---
+++ form-validate.js
@@ -0,0 +1,40 @@
+// var fname = document.getElementById('fname')
+// var lname = document.getElementById('lname')
+// var subject = document.getElementById('subject')
+// var textarea = document.querySelector('#textarea')
+// var btn = document.querySelector('.submit-btn')
+
+// var usAll = [fname, lname, subject]
+
+// for(let i=0; i<usAll.length; i++){
+//     usAll[i].addEventListener('change', (e)=>{
+//         console.log(usAll[i])
+//         if (usAll[i].value.length === 0){
+//             btn.disabled = false;
+//         }
+        
+//     })
+// }
+// lname.addEventListener('change', (e)=>{
+    
+//     if (lname.value.length === 0){
+//         btn.disabled = true;
+//     }
+// })
+// subject.addEventListener('change', (e)=>{
+    
+//     if (subject.value.length === 0){
+//         btn.disabled = false;
+//     }
+// })
+// textarea.addEventListener('change', (e)=>{
+    
+//     if (textarea.value.length === 0){
+//         btn.disabled = false;
+//     }
+// })
+
+
+
+//     console.log('helo me')
+// document.DOMContentLoaded( console.log('Hello im h'))
