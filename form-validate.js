562aaa3c993b56353dd0cfa9fca3059fce360b18
codingdrizzle <codingfrancis100@gmail.com>
2021-11-12 16:46:22 +0000

    getting finished

--- form-validate.js
+++ form-validate.js
@@ -1,40 +1,5 @@
-// var fname = document.getElementById('fname')
-// var lname = document.getElementById('lname')
-// var subject = document.getElementById('subject')
-// var textarea = document.querySelector('#textarea')
-// var btn = document.querySelector('.submit-btn')
+let messageForm = document.querySelector('#messageform');
 
-// var usAll = [fname, lname, subject]
-
-// for(let i=0; i<usAll.length; i++){
-//     usAll[i].addEventListener('change', (e)=>{
-//         console.log(usAll[i])
-//         if (usAll[i].value.length === 0){
-//             btn.disabled = false;
-//         }
-        
-//     })
-// }
-// lname.addEventListener('change', (e)=>{
-    
-//     if (lname.value.length === 0){
-//         btn.disabled = true;
-//     }
-// })
-// subject.addEventListener('change', (e)=>{
-    
-//     if (subject.value.length === 0){
-//         btn.disabled = false;
-//     }
-// })
-// textarea.addEventListener('change', (e)=>{
-    
-//     if (textarea.value.length === 0){
-//         btn.disabled = false;
-//     }
-// })
-
-
-
-//     console.log('helo me')
-// document.DOMContentLoaded( console.log('Hello im h'))
+messageForm.addEventListener('submit',()=>{
+    alert('oh okay')
+})
